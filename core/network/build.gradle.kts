700690b17f602ec8766228973b94651f4738f468
kofi-k <kofivincent026@gmail.com>
2024-08-09 13:04:25 +0000

    core: network - added network module for backend communication

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    alias(libs.plugins.crbt.android.library)
+    alias(libs.plugins.crbt.android.library.jacoco)
+    alias(libs.plugins.crbt.android.hilt)
+    id("kotlinx-serialization")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.crbtjetcompose.core.network"
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+secrets {
+    defaultPropertiesFileName = "secrets.defaults.properties"
+}
+
+dependencies {
+
+    api(libs.kotlinx.datetime)
+    api(projects.core.common)
+    api(projects.core.model)
+
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.svg)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+
+}
