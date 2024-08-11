e0234e39ef296732a6e3e808c8bb047ed04a4167
kofi-k <kofivincent026@gmail.com>
2024-08-11 00:46:39 +0000

    benchmarks for performance - initialization

---
+++ benchmark/build.gradle.kts
@@ -0,0 +1,62 @@
+import com.example.crbtjetcompose.configureFlavors
+
+plugins {
+    alias(libs.plugins.baselineprofile)
+    alias(libs.plugins.crbt.android.test)
+}
+
+android {
+    namespace = "com.example.crbtjetcompose.benchmark"
+
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "APP_BUILD_TYPE_SUFFIX", "\"\"")
+
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    configureFlavors(this) { flavor ->
+        buildConfigField(
+            "String",
+            "APP_FLAVOR_SUFFIX",
+            "\"${flavor.applicationIdSuffix ?: ""}\""
+        )
+    }
+
+    testOptions.managedDevices.devices {
+        create<com.android.build.api.dsl.ManagedVirtualDevice>("pixel6Api33") {
+            device = "Pixel 6"
+            apiLevel = 33
+            systemImageSource = "aosp"
+        }
+    }
+
+
+    targetProjectPath = ":app"
+    experimentalProperties["android.experimental.self-instrumenting"] = true
+}
+
+baselineProfile {
+    // This specifies the managed devices to use that you run the tests on.
+    managedDevices += "pixel6Api33"
+
+    // Don't use a connected device but rely on a GMD for consistency between local and CI builds.
+    useConnectedDevices = false
+
+}
+
+dependencies {
+    implementation(libs.androidx.benchmark.macro)
+    implementation(libs.androidx.test.core)
+    implementation(libs.androidx.test.espresso.core)
+    implementation(libs.androidx.test.ext)
+    implementation(libs.androidx.test.rules)
+    implementation(libs.androidx.test.runner)
+    implementation(libs.androidx.test.uiautomator)
+}
+
