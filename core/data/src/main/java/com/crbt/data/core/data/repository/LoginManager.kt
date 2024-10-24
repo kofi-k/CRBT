ed373aa40d1df0c4178f5f569a26237614883f3e
kofi-k <kofivincent026@gmail.com>
2024-10-24 23:03:59 +0000

    core: data -> login logic implementation and source for userpreference data

--- core/data/src/main/java/com/crbt/data/core/data/repository/LoginManager.kt
+++ core/data/src/main/java/com/crbt/data/core/data/repository/LoginManager.kt
@@ -6,4 +6,25 @@ interface LoginManager {
     val isLoggedIn: Flow<Boolean>
 
     suspend fun isDifferentUser(phoneNumber: String): Boolean
+
+    suspend fun login(
+        phone: String,
+        accountType: String,
+        langPref: String
+    )
+
+    suspend fun getAccountInfo()
+
+    suspend fun updateUserInfo(
+        firstName: String,
+        lastName: String,
+    ): Flow<UpdateUserInfoUiState>
+}
+
+
+sealed class UpdateUserInfoUiState {
+    data object Loading : UpdateUserInfoUiState()
+    data object Idle : UpdateUserInfoUiState()
+    data object Success : UpdateUserInfoUiState()
+    data class Error(val message: String) : UpdateUserInfoUiState()
 }
