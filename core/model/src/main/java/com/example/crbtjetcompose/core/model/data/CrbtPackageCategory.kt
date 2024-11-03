236aec278e69ef90ebed1e3239de97367383e242
kofi-k <kofivincent026@gmail.com>
2024-11-03 01:31:45 +0000

    core:model -> package model prop updates

--- core/model/src/main/java/com/example/crbtjetcompose/core/model/data/CrbtPackageCategory.kt
+++ core/model/src/main/java/com/example/crbtjetcompose/core/model/data/CrbtPackageCategory.kt
@@ -4,35 +4,4 @@ data class CrbtPackageCategory(
     val id: String,
     val packageName: String,
     val packageDescription: String,
-) {
-
-    companion object {
-        val dummyPackages = listOf(
-            CrbtPackageCategory(
-                id = "1",
-                packageName = "5G",
-                packageDescription = "5G Description",
-            ),
-            CrbtPackageCategory(
-                id = "2",
-                packageName = "Voice",
-                packageDescription = "Voice Description",
-            ),
-            CrbtPackageCategory(
-                id = "3",
-                packageName = "Internet",
-                packageDescription = "Internet Description",
-            ),
-            CrbtPackageCategory(
-                id = "4",
-                packageName = "Voice & Internet",
-                packageDescription = "Voice & Internet Description",
-            ),
-            CrbtPackageCategory(
-                id = "5",
-                packageName = "Flexi Package",
-                packageDescription = "Flexi Package Description",
-            ),
-        )
-    }
-}
+)
