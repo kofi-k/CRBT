f98df55857aeb93b1facc79f7e32273c4a341420
kofi-k <kofivincent026@gmail.com>
2024-12-20 22:50:57 +0000

    feature: profile -> bug fixes and simplified state exposing

--- feature/profile/src/main/java/com/crbt/profile/ProfileViewModel.kt
+++ feature/profile/src/main/java/com/crbt/profile/ProfileViewModel.kt
@@ -1,19 +1,22 @@
 package com.crbt.profile
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.crbt.data.core.data.phoneAuth.PhoneAuthRepository
 import com.crbt.data.core.data.phoneAuth.SignOutState
 import com.crbt.data.core.data.repository.CrbtPreferencesRepository
-import com.crbt.data.core.data.repository.LoginManager
 import com.crbt.data.core.data.repository.UpdateUserInfoUiState
+import com.crbt.data.core.data.repository.UserManager
 import com.crbt.domain.GetUserDataPreferenceUseCase
 import com.crbt.domain.UserPreferenceUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -22,16 +25,17 @@ import javax.inject.Inject
 class ProfileViewModel @Inject constructor(
     private val phoneAuthRepository: PhoneAuthRepository,
     private val crbtPreferencesRepository: CrbtPreferencesRepository,
-    private val loginManager: LoginManager,
+    private val userManager: UserManager,
     getUserDataPreferenceUseCase: GetUserDataPreferenceUseCase,
 ) : ViewModel() {
 
-    private val _userInfoUiState =
-        MutableStateFlow<UpdateUserInfoUiState>(UpdateUserInfoUiState.Idle)
-    val userInfoUiState: StateFlow<UpdateUserInfoUiState> = _userInfoUiState.asStateFlow()
+
+    var userInfoUiState by mutableStateOf<UpdateUserInfoUiState>(UpdateUserInfoUiState.Idle)
+        private set
 
     private val _signOutState = MutableStateFlow<SignOutState>(SignOutState.Idle)
     val signOutState: StateFlow<SignOutState> = _signOutState
+
 
     val userPreferenceUiState: StateFlow<UserPreferenceUiState> =
         getUserDataPreferenceUseCase()
@@ -54,22 +58,46 @@ class ProfileViewModel @Inject constructor(
     }
 
 
-    fun saveProfileImage(url: String) {
+    fun saveLanguageCode(languageCode: String) {
         viewModelScope.launch {
-            crbtPreferencesRepository.setUserProfilePictureUrl(url)
+            crbtPreferencesRepository.setUserLanguageCode(languageCode)
         }
     }
 
     fun saveProfile(
         firstName: String,
         lastName: String,
+        email: String,
+        profile: String,
+        onSuccessfulUpdate: () -> Unit,
+        onFailedUpdate: (message: String) -> Unit,
     ) {
+        userInfoUiState = UpdateUserInfoUiState.Loading
         viewModelScope.launch {
-            loginManager.updateUserInfo(
+            when (val state = userManager.updateUserInfo(
                 firstName = firstName,
-                lastName = lastName
-            ).collect {
-                _userInfoUiState.value = it
+                lastName = lastName,
+                profile = profile.ifBlank { null }
+            )) {
+                is UpdateUserInfoUiState.Success -> {
+                    crbtPreferencesRepository.updateUserPreferences(
+                        crbtPreferencesRepository.userPreferencesData.first().copy(
+                            email = email,
+                            profileUrl = profile
+                        )
+                    )
+                    onSuccessfulUpdate()
+                    userInfoUiState = state
+                }
+
+                is UpdateUserInfoUiState.Error -> {
+                    onFailedUpdate(state.message)
+                    userInfoUiState = state
+                }
+
+                else -> {
+                    userInfoUiState = state
+                }
             }
         }
     }
