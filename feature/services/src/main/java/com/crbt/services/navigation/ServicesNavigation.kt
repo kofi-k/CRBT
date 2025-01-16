b57cb47776826cc9297d520c520df8dc0973ad6c
kofi-k <kofivincent026@gmail.com>
2025-01-16 11:48:37 +0000

    feature: services, subscription -> removed unused composables, screens & vms

--- feature/services/src/main/java/com/crbt/services/navigation/ServicesNavigation.kt
+++ feature/services/src/main/java/com/crbt/services/navigation/ServicesNavigation.kt
@@ -1,22 +1,16 @@
 package com.crbt.services.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import com.crbt.services.RechargeScreen
 import com.crbt.services.ServicesRoute
-import com.crbt.services.TopUpCheckoutScreen
-import com.crbt.services.TopupScreen
 import com.crbt.services.packages.PackagesScreen
 
-const val TOPUP_AMOUNT_ARG = "topup_amount"
 const val SERVICES_ROUTE = "services_route"
-const val TOPUP_ROUTE = "$SERVICES_ROUTE/topup"
-const val TOPUP_CHECKOUT_ROUTE =
-    "$SERVICES_ROUTE/topup_checkout?$TOPUP_AMOUNT_ARG={$TOPUP_AMOUNT_ARG}"
 
 const val PACKAGES_ROUTE = "$SERVICES_ROUTE/packages"
 const val RECHARGE_ROUTE = "$SERVICES_ROUTE/recharge"
@@ -25,9 +19,7 @@ fun NavController.navigateToServices(navOptions: NavOptions) =
     navigate(SERVICES_ROUTE, navOptions)
 
 
-fun NavController.navigateToTopUpCheckout(amount: String, navOptions: NavOptions? = null) =
-    navigate("$SERVICES_ROUTE/topup_checkout?$TOPUP_AMOUNT_ARG=$amount", navOptions)
-
+@RequiresApi(Build.VERSION_CODES.O)
 fun NavGraphBuilder.servicesScreen(
     navController: NavController,
 ) {
@@ -39,31 +31,6 @@ fun NavGraphBuilder.servicesScreen(
             navigateToRecharge = {
                 navController.navigate(RECHARGE_ROUTE)
             }
-        )
-    }
-
-    composable(route = TOPUP_ROUTE) {
-        TopupScreen(
-            onTopUpClick = { amount ->
-                navController.navigateToTopUpCheckout(amount)
-            }
-        )
-    }
-
-    composable(
-        route = TOPUP_CHECKOUT_ROUTE,
-        arguments = listOf(
-            navArgument(TOPUP_AMOUNT_ARG) { type = NavType.StringType }
-        )
-    ) {
-        TopUpCheckoutScreen(
-            onCompleteTopUp = {
-                navController.navigate(SERVICES_ROUTE) {
-                    popUpTo(SERVICES_ROUTE) {
-                        inclusive = true
-                    }
-                }
-            },
         )
     }
 
